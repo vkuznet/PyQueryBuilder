c66644175c3f38f9c8b94d3df92e374e35a4abb1
liangd <liangd@localhost.localdomain>
2010-10-27 17:20:51 +0800

    seems missing a __init__.py

---
+++ pyquerybuilder/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+
+"""
+QueryBuilder module
+
+.. moduleauthor:: Valentin Kuznetsov <vkuznet@gmail.com>
+"""
+
+__revision__ = "$Id: $"
+__version__ = "$Revision: $"
+__author__ = "Valentin Kuznetsov"
+__all__ = ['qb',
+           'parser',
+           'tools',
+           'utils',
+           'config',
+           'dbsh',
+           'db',
+           'doc',
+           'web',
+           'wrappers']
+
+version = "1.0.0"
